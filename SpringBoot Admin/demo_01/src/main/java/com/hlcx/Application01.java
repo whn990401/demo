243a27e31833b82ba6e61d2e26173639bb07f743
王浩南 <whn990401@163.com>
2020-12-03 10:37:02 +0800

    admin客户端

---
+++ SpringBoot Admin/demo_01/src/main/java/com/hlcx/Application01.java
@@ -0,0 +1,15 @@
+package com.hlcx;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application01 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application01.class, args);
+    }
+}
